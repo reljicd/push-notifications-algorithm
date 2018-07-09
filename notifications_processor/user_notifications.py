97ab939bca792afa2ba3bfa3d4f2c17a63a3fc7a
Dušan Reljić <reljicd@gmail.com>
2018-07-09 12:21:29 +0200

    refactored name of some variables in user_notifications.py

--- notifications_processor/user_notifications.py
+++ notifications_processor/user_notifications.py
@@ -8,7 +8,7 @@ from notifications_processor.event import Event
 TIMESTAMP_FORMAT = '%Y-%m-%d %H:%M:%S'
 MAX_NOTIFICATIONS_PER_DAY = 4
 SECONDS_PER_6H_PERIOD = 6 * 60 * 60
-EXP_AVERAGE_WEIGHT = 0.5
+SMOOTHING_FACTOR = 0.5
 
 Notification = namedtuple('Notification', 'notification_sent, timestamp_first_tour, tours, receiver_id, message')
 
@@ -19,10 +19,10 @@ class UserNotifications(object):
         self.user_id = user_id
         self._events = []
         self._last_pushed_event_datetime = datetime.min
-        self._exp_average = 0
+        self._delay_factor = 0
 
     def add_event_and_return_notifications(self, event: Event) -> List[Notification]:
-        self._update_exp_average(event.event_datetime)
+        self._update_delay_factor(event.event_datetime)
         self._events.append(event)
         notifications = self._push_notifications()
         return notifications
@@ -51,17 +51,17 @@ class UserNotifications(object):
 
         return notifications
 
-    def _update_exp_average(self, event_datetime: datetime) -> None:
-        """ After each added event, calculates new exp_average as:
-        exp_average = exp_average * EXP_AVERAGE_WEIGHT + correction * (1 - EXP_AVERAGE_WEIGHT)
+    def _update_delay_factor(self, event_datetime: datetime) -> None:
+        """ After each added event, calculates new delay_factor as:
+        delay_factor = delay_factor * SMOOTHING_FACTOR + correction * (1 - SMOOTHING_FACTOR)
         """
         correction = self._correction(event_datetime)
-        self._exp_average = self._exp_average * EXP_AVERAGE_WEIGHT + correction * (1 - EXP_AVERAGE_WEIGHT)
+        self._delay_factor = self._delay_factor * SMOOTHING_FACTOR + correction * (1 - SMOOTHING_FACTOR)
 
     def _correction(self, event_datetime: datetime) -> float:
         """ Calculates correction as simple Heaviside step function (unit step function):
-        f(x) = (0, x >= SECONDS_PER_6H_PERIOD;
-                1, x < SECONDS_PER_6H_PERIOD)
+        f(x) = (0, x >= 6H;
+                1, x < 6H)
         """
         time_delta = event_datetime - self._last_pushed_event_datetime
         seconds_delta = time_delta.total_seconds()
@@ -72,9 +72,9 @@ class UserNotifications(object):
 
     def _correction_logarithmic(self, event_datetime: datetime) -> float:
         """ Calculates correction as:
-        f(x) = (1, x <= SECONDS_PER_6H_PERIOD / 10;
-                -log(seconds_delta/SECONDS_PER_6H_PERIOD), SECONDS_PER_6H_PERIOD / 10 < x < SECONDS_PER_6H_PERIOD;
-                0, x >= SECONDS_PER_6H_PERIOD)
+        f(x) = (1, x <= 6H/10;
+                -log(x/6H), 6H/10 < x < 6H;
+                0, x >= 6H)
         """
         time_delta = event_datetime - self._last_pushed_event_datetime
         seconds_delta = time_delta.total_seconds()
@@ -87,9 +87,9 @@ class UserNotifications(object):
 
     def _next_notification_datetime(self) -> datetime:
         """ Calculates and returns next notification datetime as:
-        next_notification_datetime = last_pushed_event_datetime + exp_average * SECONDS_PER_6H_PERIOD
+        next_notification_datetime = last_pushed_event_datetime + delay_factor * SECONDS_PER_6H_PERIOD
         """
-        return self._last_pushed_event_datetime + timedelta(seconds=SECONDS_PER_6H_PERIOD * self._exp_average)
+        return self._last_pushed_event_datetime + timedelta(seconds=SECONDS_PER_6H_PERIOD * self._delay_factor)
 
     def _friends_names(self) -> str:
         """ Util method for printing all unique friends names (sorted)
